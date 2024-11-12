439f87a6a517cf4c9769c6e05adc8b0128ad4960
carrillod5 <carrillod5@southernct.edu>
2024-11-11 23:38:13 -0500

    made connections to the server and database and modified the sql file a little but

--- public_html/js/items.js
+++ public_html/js/items.js
@@ -1,1 +1,54 @@
-alert('items.js being read')
+fetch('/getitems', {
+    method: 'GET',
+    })
+    .then(response => response.json())  // Parsing the JSON response
+    .then(data => {
+        console.log('Response from server:', data);
+
+        itemTable = document.getElementById('itemTable').getElementsByTagName("tbody")[0];
+        
+        data.forEach(item => {
+            const newRow = itemTable.insertRow();
+            const itemName = newRow.insertCell(0);
+            const itemPrice = newRow.insertCell(1);
+            const itemStock = newRow.insertCell(2);
+            const cellActions = newRow.insertCell(3);
+
+            // Fill in the cells with data
+            itemName.textContent = item.itemName;
+            itemPrice.textContent = '$'+item.price;
+            itemStock.textContent = item.stock;
+
+
+
+
+
+            // Create an Edit button
+            const editButton = document.createElement("button");
+            editButton.textContent = "Edit";
+            editButton.onclick = function() {
+                // Add logic to handle the edit action
+                // alert("for "+ customer.firstName+" "+customer.lastName)
+                openModal('editmodal')
+                document.getElementById('editItemName').value = item.itemName
+                document.getElementById('editItemPrice').value = item.price
+                document.getElementById('editItemStock').value = item.stock
+
+ 
+
+            };
+
+            // Append the Edit button to the last cell (cellActions)
+            cellActions.appendChild(editButton);
+                
+            
+        });
+        
+
+    })
+    .catch(error => {
+        console.error('Error sending data:', error);
+    });
+// }
+
+
--- server.js
+++ server.js
@@ -1,11 +1,94 @@
 const express = require('express');
 const path = require('path')
 const app = express();
+const mysql = require('mysql2');
+
 
 const port = 3000; // Change port number to something above 1024
 
+
+
+const connectionObj = {
+  host: 'localhost',
+  port:3306,
+  user: 'caruser',
+  password: 'Scsucsc540',
+  database: 'CarFixer',
+  connectionLimit:  10  
+};
+
+const pool = mysql.createPool(connectionObj);
+
+// // Query to get all tables in the database
+// pool.query('SELECT * FROM Customers', (err, results) => {
+//   if (err) {
+//     console.error('Error retrieving tables:', err.message);
+//   } else {
+//     console.log('Data in Customers Table');
+//     console.log(results);
+//   }
+//   // Close the connection pool
+//   // pool.end();
+// });
+
+
+
+
+
 // Serve static files from the 'public_html' directory
 app.use(express.static(path.join(__dirname, 'public_html')));
+
+
+
+app.get('/getitems', (req,res) =>{
+
+  pool.query('SELECT * FROM Items', (err, results) => {
+    if (err) {
+      return console.error('Error retrieving Items Table:', err.message);
+    } else {
+      console.log('Data in Items Table');
+      console.log(results);
+      return res.json(results)
+    }
+    // Close the connection pool
+    // pool.end();
+  })
+}
+);
+
+app.get('/getservices', (req,res) =>{
+  // Query to get all tables in the database
+  pool.query('SELECT * FROM Services', (err, results) => {
+    if (err) {
+      return console.error('Error retrieving Services Table:', err.message);
+    } else {
+      console.log('Data in Services Table');
+      console.log(results);
+      return res.json(results)
+    }
+    // Close the connection pool
+    // pool.end();
+  })
+}
+);
+
+
+
+app.get('/getcustomers', (req,res) =>{
+  // Query to get all tables in the database
+  pool.query('SELECT * FROM Customers', (err, results) => {
+    if (err) {
+      return console.error('Error retrieving Customers Table:', err.message);
+    } else {
+      console.log('Data in Customers Table');
+      console.log(results);
+      return res.json(results)
+    }
+    // Close the connection pool
+    // pool.end();
+  });
+})
+
 
 
 app.get('/', (req, res) => {
@@ -18,5 +101,5 @@ app.get('/:file', (req, res) => {
 });
   
 app.listen(port, '0.0.0.0', () => {
-  console.log(`Server running at http://0.0.0.0:${port}/`);
+  console.log(`Server running at port ${port}`);
 });
