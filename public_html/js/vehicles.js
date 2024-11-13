07cab22345c9acde9bcd34ce19589b4b7dedf9f2
carrillod5 <carrillod5@southernct.edu>
2024-11-13 17:45:46 -0500

    css changes

--- public_html/js/vehicles.js
+++ public_html/js/vehicles.js
@@ -1,1 +1,27 @@
-alert('vehicles.js being read')
+// alert('vehicles.js being read')
+
+
+
+fetch('/getcustomers', {
+    method: 'GET',
+})
+    .then(response => response.json())  // Parsing the JSON response
+    .then(customers => {
+
+        const customerSelect = document.getElementById("customers");
+
+        customers.forEach(customer =>{
+            const option = document.createElement('option');
+            option.value = customer.customerId
+            option.textContent = customer.customerId+". "+customer.firstName+" "+customer.lastName
+            customerSelect.appendChild(option);
+        }
+
+
+        )
+
+    })
+
+    .catch(error => {
+        console.error('Error sending data:', error);
+    });
