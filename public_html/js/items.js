e4e89516f6903c96bf237c25fa3d5965159de54b
carrillod5 <carrillod5@southernct.edu>
2024-11-20 16:27:45 -0500

    edit item done

--- public_html/js/items.js
+++ public_html/js/items.js
@@ -30,9 +30,45 @@ fetch('/getitems', {
                 // Add logic to handle the edit action
                 // alert("for "+ customer.firstName+" "+customer.lastName)
                 openModal('editmodal')
+                document.getElementById('editItemName').disabled = true;
+
                 document.getElementById('editItemName').value = item.itemName
                 document.getElementById('editItemPrice').value = item.price
                 document.getElementById('editItemStock').value = item.stock
+
+               
+                document.getElementById('edititem').onclick = function(){
+                    alert(`editing ${item.itemName}`)
+
+                    itemData = {
+                        itemName:item.itemName,
+                        itemPrice:document.getElementById('editItemPrice').value,
+                        itemStock:document.getElementById('editItemStock').value
+                    }
+                    fetch('/edititem', {
+                        method: 'POST',  // Use POST to add data to the server
+                        headers: {
+                            'Content-Type': 'application/json'  // Inform server we're sending JSON data
+                        },
+                        body: JSON.stringify(itemData)})
+                    .then(response => response.json())
+                    .then(data =>{
+                        console.log(data)
+                        if (data.message=='success'){
+                            alert("item edited")
+                            location.reload()
+            
+                        }
+                        else{
+                            alert('logical error')
+                        }
+                    })
+                    .catch(error => {
+                        console.error('Error sending data:', error);
+                    });
+                        
+
+                }
 
  
 
@@ -49,6 +85,52 @@ fetch('/getitems', {
     .catch(error => {
         console.error('Error sending data:', error);
     });
-// }
 
+    document.getElementById('addNewItem').onclick = function(){
+        alert('adding item')
+
+        newItem ={
+            itemName : document.getElementById('itemName').value,
+            itemPrice : parseFloat(document.getElementById('itemPrice').value),
+            itemStock : parseInt(document.getElementById('itemStock').value),
+        }
+        console.log(newItem);
+
+        fetch('/additem', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(newItem),
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data)
+            if (data.message=='existing'){
+                alert("Item already exists!")
+            }
+            else if (data.message=='success'){
+                alert('Item successfully added')
+                location.reload(); // Reload to reflect changes
+    
+            }
+            else{
+                alert('logical error')
+            }
+            
+         
+        })
+        .catch(error => {
+            console.error('Error adding new customer:', error);
+            alert('Failed to add item. Please try again.');
+        });
+    
+
+
+
+
+    }
 
